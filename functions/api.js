efa221c33a2056de59f0489be2c9a2cc1759c9b4
Mikayel Stepanyan <mstepanyan@vmware.com>
2022-11-30 15:18:34 +0400

    api

--- functions/api.js
+++ functions/api.js
@@ -4,46 +4,29 @@ const bcrypt = require('bcrypt');
 const uri = process.env.MongoURL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const connect = async (callback) => {
+const getAllData = async () => {
     try {
         await client.connect();
-        console.log('Connected successfully');
-        await callback();
-        client.close();
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-const getAllData = async () => {
-    await connect(async () => {
-        try {
-            const matches = await client.db('fifa22cup').collection('matches').find().toArray();
-            const results = await client.db('fifa22cup').collection('results').find().toArray();
-            return {
-                allMatches: matches,
-                allResults: results
-            }
-        } catch (e) {
-            return null;
+        const matches = await client.db('fifa22cup').collection('matches').find().toArray();
+        const results = await client.db('fifa22cup').collection('results').find().toArray();
+        return {
+            allMatches: matches,
+            allResults: results
         }
-    });
+    } catch (e) {
+        return e.toString();
+    } finally {
+        client.close();
+    }
 };
 
 const handler = async (event, context) => {
     console.log(event);
     console.log(context);
     const allData = await getAllData();
-    if (allData) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify(allData)
-        }
-    } else {
-        return {
-            statusCode: 200,
-            body: JSON.stringify([event, context, allData])
-        }
+    return {
+        statusCode: 200,
+        body: JSON.stringify([event, context, allData])
     }
 }
 

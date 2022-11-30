5e74bfcd01c2901f0dd45b613ae27c643902c56c
Mikayel Stepanyan <mstepanyan@vmware.com>
2022-12-01 03:15:46 +0400

    testing router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,3 +7,31 @@ import TheWelcome from '../components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
+
+<script>
+export default {
+  methods: {
+    getUserCookie() {
+      const cookies = document.cookie.split(';');
+      const userCookie = cookies.find((cookie) => {
+        return cookie.indexOf('username=') >= 0;
+      });
+      if (userCookie) {
+        const value = userCookie.split('=')[1];
+        const valueSet = value.split('&');
+        return {
+          username: valueSet[0],
+          alreadyLogined: valueSet[1]
+        }
+      }
+      return null;
+    }
+  },
+  mounted() {
+    const userCookie = this.getUserCookie();
+    if (userCookie === null || !userCookie.alreadyLogined) {
+      this.$router.replace({name: 'login'});
+    }
+  }
+}
+</script>

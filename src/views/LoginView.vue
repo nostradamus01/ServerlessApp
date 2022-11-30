65adb61f8093f41b8b3eb2069ba5482ff6dd0909
Mikayel Stepanyan <mstepanyan@vmware.com>
2022-12-01 02:41:44 +0400

    login, first steps

---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="main-content">
+        <div class="login-form">
+            <select class='user-select' name="username" id="username">
+                <option disabled selected hidden>Ընտրիր քեզ</option>
+                <option v-for="user in allUsers" :value="user.username">{{ user.name_tr }}</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        async allUsers() {
+            const result = await fetch('/api', {
+                method: 'POST',
+                body: JSON.stringify({
+                    action: 'getAllUsers'
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            return await result.json();
+        }
+    }
+}
+</script>
+
+<style scopet>
+.user-select {
+    padding: 10px;
+    font-size: 1.4em;
+    width: 100%;
+}
+</style>
